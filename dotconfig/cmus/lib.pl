e7f90287bfc57bdc606c1453d41ce5bbf7e6d807
acopier <acopierlive@gmail.com>
2023-02-16 07:27:26 +0100

    Add files via upload

---
+++ dotconfig/cmus/lib.pl
@@ -0,0 +1,3 @@
+/home/filip/Music/brooklynbloodpop-lyrics.mp3
+/home/filip/music/GigaChad Theme (Phonk House Version)-dp6cLZisGuA.m4a
+/home/filip/music/GigaChad Theme (Phonk House Version)-dp6cLZisGuA.opus
