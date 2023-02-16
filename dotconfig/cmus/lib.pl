873ded61931e637ac88b0ad6bdcd2a6763de3599
acopier <acopierlive@gmail.com>
2023-02-16 06:29:19 +0000

    Delete dotconfig/cmus directory

--- dotconfig/cmus/lib.pl
+++
@@ -1,3 +0,0 @@
-/home/filip/Music/brooklynbloodpop-lyrics.mp3
-/home/filip/music/GigaChad Theme (Phonk House Version)-dp6cLZisGuA.m4a
-/home/filip/music/GigaChad Theme (Phonk House Version)-dp6cLZisGuA.opus
