0e08abf8e53d91fcc374405103942b112374f887
acopier <acopierlive@gmail.com>
2023-02-27 19:58:58 +0100

    remove Server

--- Server/playit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-killall playit >/dev/null &
-exec /home/filip/Server/playit -s >/dev/null
--- Server/start.sh
+++
@@ -1,2 +0,0 @@
-./playit -s>/dev/null &
-java -Xmx640M -DPurpur.IReallyDontWantSpark=true --add-modules=jdk.incubator.vector -jar purpur-1.19.3.jar nogui
